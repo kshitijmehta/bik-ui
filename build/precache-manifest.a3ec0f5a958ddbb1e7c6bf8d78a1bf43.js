5a588658dd605b62cfed5f536216e60bb5e99a32
kshitijmehta <kshittijmehta@gmail.com>
2020-12-01 15:45:43 +0530

    added build folder

---
+++ build/precache-manifest.a3ec0f5a958ddbb1e7c6bf8d78a1bf43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f4a62f803a512a1dfb2213152a20ef9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5aa44b9552348d614aa",
+    "url": "/static/css/main.0b3ef6d4.chunk.css"
+  },
+  {
+    "revision": "c01a431e6c4c5aea6f18",
+    "url": "/static/js/2.0691efe1.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.0691efe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5aa44b9552348d614aa",
+    "url": "/static/js/main.21f8d3f9.chunk.js"
+  },
+  {
+    "revision": "e3ea2abff7a8940b6f9a",
+    "url": "/static/js/runtime-main.3a50826b.js"
+  }
+]);
