e9d5c2ceabf76ccf094a9a24c2e39ed425a6a00d
kshitijmehta <kshittijmehta@gmail.com>
2020-12-05 02:39:31 +0530

    undo width

--- build/precache-manifest.a08cab20daa40737e4b83643dfe9691a.js
+++ build/precache-manifest.a08cab20daa40737e4b83643dfe9691a.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "68bcf2092ecd0869ef72158cd2ca90ca",
+    "revision": "8c398b1131bb7b1fdc458af65642e895",
     "url": "/index.html"
   },
   {
-    "revision": "ceb1f2b30ccd2a297e58",
-    "url": "/static/css/main.230f57c3.chunk.css"
+    "revision": "9e5e9d71afaf719c761d",
+    "url": "/static/css/main.0b3ef6d4.chunk.css"
   },
   {
     "revision": "87145e52c31d65470a2b",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.2250c909.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ceb1f2b30ccd2a297e58",
+    "revision": "9e5e9d71afaf719c761d",
     "url": "/static/js/main.5fb37c5f.chunk.js"
   },
   {
