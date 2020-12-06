d2d35fc9fe5fc403c5190702584da101aec6f790
kshitijmehta <kshittijmehta@gmail.com>
2020-12-06 18:31:57 +0530

    build

--- build/precache-manifest.cf2dc1b65a50a2facea655cd47b69972.js
+++ build/precache-manifest.cf2dc1b65a50a2facea655cd47b69972.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "efa5ff43f8e7935a425f4277b4800be6",
+    "revision": "ed418abbd55a0a24f35634761cb3a355",
     "url": "/index.html"
   },
   {
-    "revision": "8cf8bac095d3b3eebd62",
-    "url": "/static/css/main.ff75953f.chunk.css"
+    "revision": "0b3e93d0f894a8b8b073",
+    "url": "/static/css/main.49f376e0.chunk.css"
   },
   {
     "revision": "87145e52c31d65470a2b",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.2250c909.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8cf8bac095d3b3eebd62",
-    "url": "/static/js/main.857c49bb.chunk.js"
+    "revision": "0b3e93d0f894a8b8b073",
+    "url": "/static/js/main.479d3846.chunk.js"
   },
   {
     "revision": "e3ea2abff7a8940b6f9a",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8d3df3fca0b699e8f33874573a3516d6.js"
+  "/precache-manifest.cf2dc1b65a50a2facea655cd47b69972.js"
 );
 
 self.addEventListener('message', (event) => {
