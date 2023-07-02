5f19259fae290ee9cd927eb256f440559f8f6973
kshitijmehta <kshittijmehta@gmail.com>
2023-07-02 23:58:56 +0530

    'updatebaseurl'

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fc808303833ca74292a172bb3ce06174.js"
+  "/precache-manifest.865e0d16e936f9f82ccd1850ef47ea48.js"
 );
 
 self.addEventListener('message', (event) => {
--- src/services/axiosBase.ts
+++ src/services/axiosBase.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 export default axios.create({
-  baseURL: 'https://basickart.com/v1',
+  baseURL: 'https://api.basickart.com/v1',
   headers: {
     'Accept': 'application/json;charset=UTF-8',
     'Authorization': ''
